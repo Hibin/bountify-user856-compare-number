43415e250d62f27984ef9ff66989d15d860b20ec
Dmitry <dmitry@wildev.ru>
2015-03-11 14:07:43 +0300

    Working version

--- compare.js
+++ compare.js
@@ -4,74 +4,49 @@ var read            = require( 'fs-readdir-recursive' )
     , fs            = require( 'fs' )
     , minimatch     = require( 'minimatch' )
     , cheerio       = require( 'cheerio' )
-    , ColorThief    = require( 'color-thief' )
-    , $colorThief   = new ColorThief()
-    , http          = require( 'http' );
-
-var $targetPath = process.argv[2];
+    , linebyline    = require( 'linebyline' )
+    , $targetPath   = process.argv[2]
+    , $targetFile   = process.argv[3]
+    ;
 
 var isHtml = function( $fileName ) {
     return minimatch( $fileName, '*.html' );
 };
 
-var toHex = function( $red, $green, $blue ) {
-    return ( ( $blue | $green << 8 | $red << 16 ) | 1 << 24 ).toString( 16 ).slice( 1 );
-};
+var $ranges = new Array();
+var $lbl = linebyline( $targetFile );
 
-var download = function( $url, $dest, $callback ) {
-    var $file    = fs.createWriteStream( $dest );
-    var $request = http.get( $url, function( $response ) {
-        $response.pipe( $file );
+$lbl.on( 'line', function( $line ) {
+    var myArray = /^(\d+)_(\d+);/.exec( $line );
 
-        $file.on(
-            'finish'
-            , function() {
-                $file.close( $callback );
-            }
-        );
+    myArray['min'] = parseFloat( myArray[1] );
+    myArray['max'] = parseFloat( myArray[2] );
 
-        $file.on(
-            'error'
-            , function( $err ) {
-                fs.unlink( $dest );
+    $ranges.push( myArray );
+});
 
-                if( $callback )
-                    $callback( $err.message );
-            }
-        );
-    });
-};
+$lbl.on( 'end', function() {
+    console.log($ranges);
+    var $files = read( $targetPath, isHtml );
 
-var $files = read( $targetPath, isHtml );
-
-$files.forEach( function( $file ) {
-    var $fileName = $targetPath + '/' + $file;
-
-    fs.readFile( $fileName, function( $err, $data ) {
-        if( $err ) throw $err;
-
-        $ = cheerio.load( $data );
-
-        var $imgSrc    = $( 'div#productImage' ).text();
-        var $imgTarget = './image-' + process.pid + '.jpg';
-
-        //fs.unlinkSync( $imgTarget );
-
-        download( $imgSrc, $imgTarget, function() {
-            $imgSrc = $imgTarget;
-
-            fs.exists( $imgSrc, function( $exists ) {
-                if( $exists ) {
-                    var $color = $colorThief.getColor( $imgSrc );
-                        $color = toHex( $color[0], $color[1], $color[2] );
+    $files.forEach( function( $file ) {
+        var $fileName = $targetPath + '/' + $file;
     
-                    fs.appendFile( $fileName, "\n" + '<div id="primary_color">#' + $color + '</div>', function( $err ) {
+        fs.readFile( $fileName, function( $err, $data ) {
+            if( $err ) throw $err;
+    
+            $ = cheerio.load( $data );
+    
+            var $finalPrice = parseFloat( $( 'div#final_price' ).text() );
+
+            // compare price with list
+            $ranges.forEach( function( $range ) {
+               if( $finalPrice >= $range['min'] && $finalPrice <= $range['max'] ) {
+                   fs.appendFile( $fileName, "\n" + '<div id="price_range">' + $range[1] + '_' + $range[2] + '</div>', function( $err ) {
                       if( $err ) throw $err;
-    
-                      console.log( $imgSrc + ' : ' + $color );
                     });
-                }
-            });
-        } );
-    });
-} );
+               }
+            } );
+        });
+    } );
+});
